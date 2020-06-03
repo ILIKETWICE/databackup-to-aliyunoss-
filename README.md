a0a547aa5e77847aae9e5bbbf8502324a1bde07f
望翔 <xiang.wang@qingclass.com>
2020-06-03 10:29:46 +0800

    first

---
+++ README.md
@@ -0,0 +1,104 @@
+#  项目简介
+
+**databackup-to-aliyunoss 是一个本地数据上传到阿里OSS的Python脚本。**
+
+**主要作用是：各个事业部下各项目组的Mysql、MongoDB备份数据分别使用此脚本上传到阿里OSS对应的bucket下的对应文件夹下。**
+
+# 项目文件解读
+
+- aliyun-oss-upload.py ：这个是上传数据所需脚本，直接使用即可，不需要改动。
+- config.txt ：这个是需要改动的文件，有阿里的AK/SK、OSS的endpoint、bucket名称
+```shell
+[common]
+# access_id
+access_id = xxxxxxxxxxxxxxxxxxx   # AK/SK 需要修改，需要企业微信运维工单申请
+
+# secret_key
+secret_key = xxxxxxxxxxxxxxxxxxx
+
+[oss]
+# OSS endpoint
+endpoint = oss-cn-beijing-internal.aliyuncs.com   
+# endpoint不用修改，但使用的服务器必须也在华北2区，否则需要使用公网endpoint，会产生额外的流量费用
+
+
+# OSS bucketname
+bucket_name = test   
+# 修改成你需要上传到的bucket名称，每个事业部不同，请查看本项目图片文件确认
+```
+
+# 命令结构
+
+```shell
+python3 aliyun-oss-upload.py 阿里oss路径 本地文件路径 企业微信机器人提醒
+```
+
+# 使用教程
+
+## 运行环境
+
+- Python3+ 
+- Python模块：oss2、requests、configparser、json
+
+
+
+##  环境准备
+
+1. 安装Python3.7环境 （安装步骤不再详细介绍，具体请查看官网https://www.python.org)
+2. 安装脚本所需模块
+
+```shell
+pip3 install oss2 requests configparser json
+```
+
+3. 查看本文档中的**阿里OSS bucket名称及目录名称**，确认你需要上传到的bucket名称及文件夹名称。
+
+
+
+## 使用示例
+
+```shell
+python3 aliyun-oss-upload.py test/mongo/apache-jmeter-5.1.1.tgz /root/apache-jmeter-5.1.1.tgz delta
+```
+
+**参数解析：**
+
+- Python3：本项目需要使用Python3以上版本，示例使用的为Python3.7
+- aliyun-oss-upload.py ：数据上传脚本文件
+- deltatest/mongo/apache-jmeter-5.1.1.tgz ：因为bucket名称在config.txt文件中确定了，所以这里只需要写bucket下的目录路径
+- /root/apache-jmeter-5.1.1.tgz ：需要上传的文件路径
+- delta ：企业微信机器人通知消息，上传成功会发消息提醒，请写你的项目组名称
+
+![JbBIdU.png](https://s1.ax1x.com/2020/04/30/JbBIdU.png)
+
+# 阿里OSS bucket名称及目录名称
+
+**阿里OSS bucket名称及目录名称的命名**根据事业部及项目组来命名，如下图：
+
+![JqnKUO.png](https://s1.ax1x.com/2020/04/30/JqnKUO.png)
+
+**相对应的bucket名称和目录名称，如下图：**
+
+![Jq1xW8.png](https://s1.ax1x.com/2020/04/30/Jq1xW8.png)
+
+# 子账号授权
+
+**各个事业部分别使用不同的子账号，授权对应的bucket只上传权限**
+
+```json
+{
+    "Version": "1",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "oss:Put*"
+            ],
+            "Resource": [
+                "acs:oss:*:*:bucket名称/*"
+            ],
+            "Condition": {}
+        }
+    ]
+}
+```
---
+++ aliyun-oss-upload.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-04-26 17:39
+# @Author  : wxl
+# @Site    : 
+# @File    : aliyun-oss-upload.py
+# @Software: PyCharm
+
+"""
+    实现的功能点如下：
+    1. 列出相关现有的项目目录(在权限访问内)
+    @. 暂时关掉写的权限，比如创建相关业务目录，在细分MySQL，MongoDB等不同的数据库
+    @. 暂时关闭压缩备份文件的功能，在各个项目中shell来实现
+    2. 上传到制定目录(检查文件是否压缩)
+
+    使用方法 xxx.py objectName localfilePath projectName
+    objectname:  delta/mysql/sql.zip
+    localfilepath:  /opt/backup/mysql/sql.zip
+    projectName: yiban
+"""
+
+import os,sys,zipfile
+import oss2
+import configparser
+import requests
+import json
+import datetime,time
+
+# 显示上传百分比，任务后台执行则可不用
+# def percentage(consumed_bytes, total_bytes):
+#     if total_bytes:
+#         rate = int(100 * (float(consumed_bytes) / float(total_bytes)))
+#         print('\r{0}% '.format(rate), end=)
+#         sys.stdout.flush()
+
+# 微信机器人汇报上传结果
+def wx_callback(content):
+    wx_url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=8f4e9a5f-9ffc-46b0-82b5-f6d5fdca7d71'
+    """艾特全部，并发送指定信息"""
+    message = content + ' upload success'
+    data = json.dumps({"msgtype": "text", "text": {"content": message, "mentioned_list": ["@all"]}})
+    r = requests.post(wx_url, data, auth=('Content-Type', 'application/json'))
+
+
+# 时间消耗
+def print_cost_time(msg, begin_time):
+	end_time = time.time()
+	cost = "%.2f" % (end_time - begin_time)
+	print(" %s cost:%s s" % (msg, cost))
+
+# # 压缩功能
+# def zip_files(dirPath, zipFilePath=None, includeDirInZip=True):
+# 	start_time = time.time();
+# 	if not zipFilePath:
+# 		zipFilePath = dirPath + ".zip";
+# 	if not os.path.isdir(dirPath):
+# 		raise OSError("dirPath "+dirPath+" does not.");
+# 	parentDir, dirToZip = os.path.split(dirPath)
+# 	#Little nested function to prepare the proper archive path
+# 	def trimPath(path):
+# 		archivePath = path.replace(parentDir, "", 1)
+# 		#if parentDir:
+# 		#	archivePath = archivePath.replace(os.path.sep, "", 1)
+# 		if not includeDirInZip:
+# 			archivePath = archivePath.replace(dirToZip + os.path.sep, "", 1);
+# 		return os.path.normcase(archivePath)
+# 	outFile = zipfile.ZipFile(zipFilePath,"w",compression=zipfile.ZIP_DEFLATED,allowZip64=True)
+# 	for(archiveDirPath,dirNames,fileNames) in os.walk(dirPath):
+# 		for fileName in fileNames:
+# 			filePath=os.path.join(archiveDirPath,fileName);
+# 			outFile.write(filePath, trimPath(filePath));
+#         #Make sure we get empty directories as well
+#         if not fileNames and not dirNames:
+#             zipInfo = zipfile.ZipInfo(trimPath(archiveDirPath) + "/")
+#             #some web sites suggest doing
+#             #zipInfo.external_attr = 16
+#             #or
+#             #zipInfo.external_attr = 48
+#             #Here to allow for inserting an empty directory.  Still TBD/TODO.
+#             outFile.writestr(zipInfo, "");
+# 	outFile.close()
+# 	print_cost_time("zip files", start_time)
+#
+# def tar_files(dirPath, zipFilePath):
+# 	spath=os.path.split(dirPath)
+# 	parentDirPath=spath[0]
+# 	dirName=spath[1]
+# 	start_time = time.time()
+# 	os.system("tar -C %s -zcvf %s %s"
+# 		%(parentDirPath,zipFilePath,dirName))
+# 	print_cost_time("tar files", start_time)
+
+
+# 定义上传类
+class UploadFile2OSS():
+    def __init__(self, objectname, file):
+        self.objectname = objectname
+        self.file = file
+        self.ossBucket = self.initOSS()
+
+    def initOSS(self):
+        '''
+        OSS初始化
+        :return:
+        '''
+        access_key_id     = self.readConfigContent('common', 'access_id')
+        access_key_secret = self.readConfigContent('common', 'secret_key')
+        oss_endpoint      = self.readConfigContent('oss', 'endpoint')
+        oss_bucket_name   = self.readConfigContent('oss', 'bucket_name')
+
+        # OSS认证
+        auth = oss2.Auth(access_key_id, access_key_secret)
+        bucket = oss2.Bucket(auth, oss_endpoint, oss_bucket_name)
+        return bucket
+
+    def readConfigContent(self, contentid, keyValue):
+        '''
+
+        :param content: 配置文件中的一级字段
+        :param keyValue: key字段
+        :return:
+
+        '''
+
+        configFile = os.getcwd() + '/config.txt'
+        print(configFile)
+        cf = configparser.ConfigParser()
+        cf.read(configFile)
+        value = cf.get(contentid, keyValue)
+        return value
+
+    def percentage(self,consumed_bytes, total_bytes):
+        if total_bytes:
+            rate = int(100 * (float(consumed_bytes) / float(total_bytes)))
+            print('\r{0}% '.format(rate), end=self.file)
+            sys.stdout.flush()
+
+    def uploadFile(self):
+        try:
+            oss2.resumable_upload(self.ossBucket, self.objectname, self.file,
+                                  multipart_threshold=100 * 1024,
+                                  num_threads=4,
+                                  progress_callback=self.percentage)
+        except Exception as e:
+            print(e)
+
+
+
+if __name__ == '__main__':
+    # 脚本需要传入3个参数
+    if (len(sys.argv) > 3):
+        obejctName  = sys.argv[1]
+        filePath    = sys.argv[2]
+        projectName = sys.argv[3]
+        # fileName = filePath.split("/")[-1]
+    else:
+        print("Example: %s  objectname  /data/backup.zip delta" % sys.argv[0])
+        exit()
+
+    uploader = UploadFile2OSS(obejctName, filePath)
+    uploader.uploadFile()
+
+    # 此处需要添加上传成功的判断
+    wx_callback(projectName)
+
+
+
+
+
+
+
